c6d09c43d91531728bd8611545b448fbcba18a1f
Alex Kordys <akordys@web3tech.ru>
2021-09-24 08:04:51 +0300

    Formatting macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,14 @@
-//! Main crate
+//! Procedural macros for formatting and parsing fragmented strings.
+//!
+//! This is the main crate which reexports macros from implementation crates.
+//!
+//! # Formatting
+//!
+//! ```
+//! use fragstrings::frag_format;
+//! let foo = "foo";
+//! let result = frag_format!("%s%s%d", foo, "bar", 42);
+//! assert_eq!(result, "%s%s%d__foo__bar__42");
+//! ```
+
+pub use format_procmacro::frag_format;
---
+++ tests/format-tests.rs
@@ -0,0 +1,47 @@
+use format_procmacro::frag_format;
+
+#[test]
+fn test_frag_format() {
+    // frag_format!(); // Compile error
+    // frag_format!(""); // Compile error
+    // frag_format!("%s"); // Compile error
+    // frag_format!("%s" foo, "%s__test"); // Compile error
+    // frag_format!("%d%d", 42); // Compile error
+    // frag_format!(42); // Compile error
+    // frag_format!(xxx); // Compile error
+    // frag_format!("xxx"); // Compile error
+
+    assert_eq!(frag_format!("%s", "test"), "%s__test");
+    assert_eq!(frag_format!("%d", 42), "%d__42");
+
+    let data_int = 42;
+    let data_str = "test";
+    let data_string = "test".to_string();
+    let frag_int = frag_format!("%d", data_int);
+    let frag_str = frag_format!("%s", data_str);
+    let frag_string = frag_format!("%s", data_string);
+    assert_eq!(frag_int, "%d__42");
+    assert_eq!(frag_str, "%s__test");
+    assert_eq!(frag_string, "%s__test");
+
+    assert_eq!(frag_format!(r"%s", "test"), "%s__test");
+    assert_eq!(frag_format!(r#"%s"#, "test"), "%s__test");
+    assert_eq!(frag_format!(r##"%s"##, "test"), "%s__test");
+    assert_eq!(frag_format!(r###"%s"###, "test"), "%s__test");
+    assert_eq!(frag_format!(b"%s", "test"), "%s__test");
+
+    assert_eq!(frag_format!("%d", 2 + 2), "%d__4");
+    assert_eq!(frag_format!("%d", (2 + 2) * 2), "%d__8");
+    assert_eq!(frag_format!("%d", int_fn(1, 2) * 3), "%d__9");
+
+    assert_eq!(frag_format!("%s%d", "test", 42), "%s%d__test__42");
+    assert_eq!(frag_format!("%d%s", 42, "test"), "%d%s__42__test");
+
+    assert_eq!(frag_format!(/* Comment */ "%s", "test"), "%s__test");
+    assert_eq!(frag_format!("%s" /* Comment */, "test"), "%s__test");
+    assert_eq!(frag_format!("%s", "test" /* Comment */), "%s__test");
+}
+
+fn int_fn(a: i32, b: i32) -> i32 {
+    a + b
+}
